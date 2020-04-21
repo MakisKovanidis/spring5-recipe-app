299e8ffad84919a866cf54459abe039074eecc66
Makis <makis_kovanidis@hotmail.com>
2020-04-21 15:14:12 +0300

    Enumaration DropDowns with springMVC and thymeleaf

--- src/main/java/com/kovanidis/spring5recipeapp/services/IngredientService.java
+++ src/main/java/com/kovanidis/spring5recipeapp/services/IngredientService.java
@@ -5,7 +5,7 @@ import com.kovanidis.spring5recipeapp.commands.IngredientCommand;
 public interface IngredientService {
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
 
-//    IngredientCommand saveIngredientCommand(IngredientCommand command);
-//
-//    void deleteById(Long recipeId, Long idToDelete);
+    IngredientCommand saveIngredientCommand(IngredientCommand command);
+
+    void deleteById(Long recipeId, Long idToDelete);
 }
